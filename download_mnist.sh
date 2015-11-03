a37d391f2d3c20913353714d42b86514d6fca880
yhc <haochaoying@126.com>
2015-11-03 10:24:53 +0800

    add the datasets

---
+++ download_mnist.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+mkdir -p ./datasets/mnist
+
+if ! [ -e ./mnist/train-images-idx3-ubyte.gz]
+    then
+        wget -P ./datasets/mnist/ http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+fi
+
+gzip -d ./datasets/mnist/train-images-idx3-ubyte.gz
+
+if ! [ -e ./datasets/mnist/train-labels-idx1-ubyte.gz ]
+	then
+		wget -P ./datasets/mnist/ http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+fi
+
+gzip -d ./datasets/mnist/train-labels-idx1-ubyte.gz
+
+if ! [ -e ./datasets/mnist/t10k-images-idx3-ubyte.gz ]
+	then
+		wget -P ./datasets/mnist/ http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+fi
+gzip -d ./datasets/mnist/t10k-images-idx3-ubyte.gz
+
+if ! [ -e ./datasets/mnist/t10k-labels-idx1-ubyte.gz ]
+	then
+		wget -P ./datasets/mnist/ http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+fi
+gzip -d ./datasets/mnist/t10k-labels-idx1-ubyte.gz
